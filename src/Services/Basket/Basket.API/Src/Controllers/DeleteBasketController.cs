919358e5c31bc21b5d1ade7d634159378255e4f7
Jan Vodák <janvodak92@gmail.com>
2023-10-01 14:25:23 +0200

    created simple basket API microservice with Redis as storage and distributed cache

---
+++ src/Services/Basket/Basket.API/Src/Controllers/DeleteBasketController.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using Basket.API.Src.Entities;
+using Basket.API.Src.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Src.Controllers
+{
+	[ApiController]
+	[Route("api/v1/basket/[controller]")]
+	[Produces("application/json")]
+	public class DeleteBasketController : ControllerBase
+	{
+		private readonly IBasketRepository _repository;
+
+		public DeleteBasketController(IBasketRepository repository)
+		{
+			this._repository = repository;
+		}
+
+		[HttpDelete("{userName}", Name = "DeleteBasket")]
+		[ProducesResponseType(typeof(BasketEntity), (int)HttpStatusCode.NoContent)]
+		[ProducesResponseType((int)HttpStatusCode.NotFound)]
+		public async Task<IActionResult> DeleteBasket(string userName)
+		{
+			await this._repository.DeleteBasket(userName);
+
+			return NoContent();
+		}
+	}
+}
---
+++ src/Services/Basket/Basket.API/Src/Controllers/UpdateBasketController.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using Basket.API.Src.Entities;
+using Basket.API.Src.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Src.Controllers
+{
+	[ApiController]
+	[Route("api/v1/basket/[controller]")]
+	[Produces("application/json")]
+	public class UpdateBasketController : ControllerBase
+	{
+		private readonly IBasketRepository _repository;
+
+		public UpdateBasketController(IBasketRepository repository)
+		{
+			this._repository = repository;
+		}
+
+		[HttpPost]
+		[ProducesResponseType(typeof(BasketEntity), (int)HttpStatusCode.OK)]
+		public async Task<ActionResult<BasketEntity>> UpdateBasket([FromBody] BasketEntity basket)
+		{
+			await this._repository.UpdateBasket(basket);
+
+			return CreatedAtRoute("GetBasket", new { userName = basket.UserName }, basket);
+		}
+	}
+}
