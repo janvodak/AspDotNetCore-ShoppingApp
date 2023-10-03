ab10744282e732a86b4ba9d3429720c29840a2d1
Jan Vodák <janvodak92@gmail.com>
2023-10-03 14:12:15 +0200

    added portainer for docker management

--- scripts/dc.sh
+++ scripts/dc.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+WITH_PORTAINER=${WITH_PORTAINER:-0}  # 0 = no, 1 = yes
+
 _eval() {
 	local command=("$@")
 	echo ">>> ${command[*]}"
@@ -20,6 +22,11 @@ _get_docker_compose_file_names() {
 		-f "${SCRIPT_DIR}/../src/docker-compose.yml"
 		-f "${SCRIPT_DIR}/../src/docker-compose.override.yml"
 	)
+
+	# Check if an additional file should be added to run portainer
+    if [[ "${WITH_PORTAINER}" == "1" ]]; then
+        filenames+=(-f "${SCRIPT_DIR}/../src/docker-compose.portainer.yml")
+    fi
 
 	echo "${filenames[@]}"
 }
