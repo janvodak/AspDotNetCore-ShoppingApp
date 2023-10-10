effea34cb38e0db4bbb400eb46615a91f4423899
Jan Vodák <janvodak92@gmail.com>
2023-10-10 13:31:41 +0200

    created discount grpc microservice

---
+++ src/Services/Discount/DIscount.Grpc/Src/Data/DatabaseSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace Discount.Grpc.Src.Data;
+
+public class DatabaseSettings
+{
+	public string User { get; set; } = null!;
+
+	public string Password { get; set; } = null!;
+
+	public string Host { get; set; } = null!;
+
+	public string Port { get; set; } = null!;
+
+	public string DBname { get; set; } = null!;
+}
