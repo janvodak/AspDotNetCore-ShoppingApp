bdf6f044b25796cf9a4e5b7c48b738f83cd9e14e
Jan Vodák <janvodak92@gmail.com>
2023-10-11 15:11:55 +0200

    added Discount Grpc clien into Basket microservice APIt

--- src/Services/Basket/Basket.API/Src/Controllers/UpdateBasketController.cs
+++ src/Services/Basket/Basket.API/Src/Controllers/UpdateBasketController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Net;
 using Basket.API.Src.Entities;
+using Basket.API.Src.GrpcServices;
 using Basket.API.Src.Repositories;
+using Discount.Grpc.Src.Protos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Basket.API.Src.Controllers
@@ -11,16 +13,25 @@ namespace Basket.API.Src.Controllers
 	public class UpdateBasketController : ControllerBase
 	{
 		private readonly IBasketRepository _repository;
+		private readonly GetDiscountGrpcService _getDiscountGrpcService;
 
-		public UpdateBasketController(IBasketRepository repository)
+		public UpdateBasketController(IBasketRepository repository, GetDiscountGrpcService getDiscountGrpcService)
 		{
 			this._repository = repository;
+			this._getDiscountGrpcService = getDiscountGrpcService;
 		}
 
 		[HttpPost]
 		[ProducesResponseType(typeof(BasketEntity), (int)HttpStatusCode.OK)]
 		public async Task<ActionResult<BasketEntity>> UpdateBasket([FromBody] BasketEntity basket)
 		{
+			foreach (var product in basket.Products)
+			{
+				GetDiscountProtocolBufferEntity discount = await this._getDiscountGrpcService.GetDiscount(product.Name);
+
+				product.Price -= discount.Amount;
+			}
+
 			await this._repository.UpdateBasket(basket);
 
 			return Ok(basket);
---
+++ src/Services/Basket/Basket.API/Src/GrpcServices/GetDiscountGrpcService.cs
@@ -0,0 +1,22 @@
+﻿
+using Discount.Grpc.Src.Protos;
+
+namespace Basket.API.Src.GrpcServices
+{
+	public class GetDiscountGrpcService
+	{
+		private readonly GetDiscountProtocolBufferService.GetDiscountProtocolBufferServiceClient _getDiscountProtocolBufferService;
+
+		public GetDiscountGrpcService(GetDiscountProtocolBufferService.GetDiscountProtocolBufferServiceClient getDiscountProtocolBufferService)
+		{
+			this._getDiscountProtocolBufferService = getDiscountProtocolBufferService;
+		}
+
+		public async Task<GetDiscountProtocolBufferEntity> GetDiscount(string productName)
+		{
+			GetDiscountRequest discountRequest = new() { ProductName = productName };
+
+			return await this._getDiscountProtocolBufferService.GetDiscountAsync(discountRequest);
+		}
+	}
+}
