e8cf0a77a6134abb53575731ce8c6f480414a05e
Jan Vodák <janvodak92@gmail.com>
2023-10-12 16:09:41 +0200

    added logic to handle use case when there is no product discount returned from discount grpc

--- src/Services/Basket/Basket.API/Src/GrpcServices/GetDiscountGrpcService.cs
+++ src/Services/Basket/Basket.API/Src/GrpcServices/GetDiscountGrpcService.cs
@@ -1,22 +1,43 @@
 ﻿
 using Discount.Grpc.Src.Protos;
+using Grpc.Core;
 
 namespace Basket.API.Src.GrpcServices
 {
 	public class GetDiscountGrpcService
 	{
+		private readonly ILogger<GetDiscountGrpcService> _logger;
 		private readonly GetDiscountProtocolBufferService.GetDiscountProtocolBufferServiceClient _getDiscountProtocolBufferService;
 
-		public GetDiscountGrpcService(GetDiscountProtocolBufferService.GetDiscountProtocolBufferServiceClient getDiscountProtocolBufferService)
+		public GetDiscountGrpcService(
+			ILogger<GetDiscountGrpcService> logger,
+			GetDiscountProtocolBufferService.GetDiscountProtocolBufferServiceClient getDiscountProtocolBufferService)
 		{
+			_logger = logger;
 			this._getDiscountProtocolBufferService = getDiscountProtocolBufferService;
 		}
 
-		public async Task<GetDiscountProtocolBufferEntity> GetDiscount(string productName)
+		public async Task<GetDiscountProtocolBufferEntity?> GetDiscount(string productName)
 		{
 			GetDiscountRequest discountRequest = new() { ProductName = productName };
 
-			return await this._getDiscountProtocolBufferService.GetDiscountAsync(discountRequest);
+			try
+			{
+				return await this._getDiscountProtocolBufferService.GetDiscountAsync(discountRequest);
+			}
+			catch (RpcException exception)
+			{
+				if (exception.Status.StatusCode == StatusCode.NotFound)
+				{
+					this._logger.LogInformation($"Product '{productName}' has no discounts.");
+				}
+				else
+				{
+					this._logger.LogError($"Unable to get discounts for product '{productName}' due to error: '{exception.Message}'");
+				}
+			}
+
+			return null;
 		}
 	}
 }
