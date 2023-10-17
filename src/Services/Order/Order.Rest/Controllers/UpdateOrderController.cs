ed0c06341eb837524b3c56e16a5635437291ac85
Jan Vodák <janvodak92@gmail.com>
2023-10-17 16:13:24 +0200

    designed and implemented controllers for rest layer in Order microservice

---
+++ src/Services/Order/Order.Rest/Controllers/UpdateOrderController.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Order.Application.Src.Features.Order.Commands.UpdateOrder;
+
+namespace Order.Rest.Controllers
+{
+	[ApiController]
+	[Route("api/v1/order/[controller]")]
+	[Produces("application/json")]
+	public class UpdateOrderController : ControllerBase
+	{
+		private readonly IMediator _mediator;
+
+		public UpdateOrderController(IMediator mediator)
+		{
+			this._mediator = mediator;
+		}
+
+		[HttpPut(Name = "UpdateOrder")]
+		[ProducesResponseType((int)HttpStatusCode.NoContent)]
+		[ProducesResponseType((int)HttpStatusCode.NotFound)]
+		[ProducesDefaultResponseType]
+		public async Task<ActionResult> UpdateOrder([FromBody] UpdateOrderCommand command)
+		{
+			await this._mediator.Send(command);
+
+			return NoContent();
+		}
+	}
+}
+
