d4e65994c32db048095ebfd628eff2e875b9032f
Jan Vodák <janvodak92@gmail.com>
2023-10-17 16:13:24 +0200

    designed and implemented controllers for rest layer in Order microservice

---
+++ src/Services/Order/Order.Rest/Controllers/CheckoutOrderController.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Order.Application.Src.Features.Order.Commands.CheckoutOrder;
+
+namespace Order.Rest.Controllers
+{
+	[ApiController]
+	[Route("api/v1/order/[controller]")]
+	[Produces("application/json")]
+	public class CheckoutOrderController : ControllerBase
+	{
+		private readonly IMediator _mediator;
+
+		public CheckoutOrderController(IMediator mediator)
+		{
+			this._mediator = mediator;
+		}
+
+		[HttpPost(Name = "CheckoutOrder")]
+		[ProducesResponseType((int)HttpStatusCode.OK)]
+		public async Task<ActionResult<int>> CheckoutOrder([FromBody] CheckoutOrderCommand command)
+		{
+			int result = await this._mediator.Send(command);
+
+			return Ok(result);
+		}
+	}
+}
