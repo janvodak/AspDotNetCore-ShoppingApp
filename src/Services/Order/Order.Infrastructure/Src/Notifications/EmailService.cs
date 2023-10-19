428cb3b635ad1fdecb8a5c5e79b0c819ebfc18f8
Jan Vodák <janvodak92@gmail.com>
2023-10-19 15:03:46 +0200

    implemented components for Infrastructure layer in Order microservice

---
+++ src/Services/Order/Order.Infrastructure/Src/Notifications/EmailService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Order.Application.Src.Contracts.Notifications;
+using Order.Application.Src.Models;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace Order.Infrastructure.Src.Notifications
+{
+	public class EmailService : IEmailService
+	{
+		private readonly EmailSettings _emailSettings;
+		private readonly ILogger<EmailService> _logger;
+
+		public EmailService(IOptions<EmailSettings> emailSettings, ILogger<EmailService> logger)
+		{
+			this._emailSettings = emailSettings.Value;
+			this._logger = logger;
+		}
+
+		public async Task<bool> SendEmail(Email email)
+		{
+			SendGridClient client = new(this._emailSettings.ApiKey);
+
+			string subject = email.Subject;
+			EmailAddress to = new(email.To);
+			string body = email.Body;
+
+			EmailAddress from = new()
+			{
+				Email = this._emailSettings.FromAddress,
+				Name = this._emailSettings.FromName
+			};
+
+			SendGridMessage sendGridMessage = MailHelper.CreateSingleEmail(from, to, subject, body, body);
+
+			var response = await client.SendEmailAsync(sendGridMessage);
+
+			if (response.StatusCode is System.Net.HttpStatusCode.Accepted
+				|| response.StatusCode is System.Net.HttpStatusCode.OK)
+			{
+				this._logger.LogInformation("Email '{Email}' was successfully sent.", email.ToString());
+				return true;
+			}
+
+			this._logger.LogError("Unable to send email '{Email}' due to error.", email.ToString());
+			return false;
+		}
+	}
+}
+
---
+++ src/Services/Order/Order.Infrastructure/Src/Persistence/Context/OrderContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Order.Application.Src.Models;
+using Order.Domain.Src.Order.Entities;
+using Order.Domain.Src.Shared;
+
+namespace Order.Infrastructure.Src.Persistence.Context
+{
+	public class OrderContext : DbContext
+	{
+		public virtual DbSet<OrderEntity> Orders { get; set; } = null!;
+
+		private readonly string _connectionString;
+
+		public OrderContext(IOptions<DatabaseSettings> databaseSettings)
+		{
+			this._connectionString = String.Format(
+				"User ID={0};Password={1};Host={2};Database={3};",
+				databaseSettings.Value.User,
+				databaseSettings.Value.Password,
+				databaseSettings.Value.Host,
+				databaseSettings.Value.DBname);
+		}
+
+		public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+		{
+			foreach (var entry in ChangeTracker.Entries<EntityBase>())
+			{
+				switch (entry.State)
+				{
+					case EntityState.Added:
+						entry.Entity.CreatedDate = DateTime.Now;
+						entry.Entity.CreatedBy = "swn";
+						break;
+					case EntityState.Modified:
+						entry.Entity.LastModifiedDate = DateTime.Now;
+						entry.Entity.LastModifiedBy = "swn";
+						break;
+					default:
+						break;
+				}
+			}
+
+			return base.SaveChangesAsync(cancellationToken);
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+		{
+			dbContextOptionsBuilder.UseSqlServer(this._connectionString);
+		}
+	}
+}
