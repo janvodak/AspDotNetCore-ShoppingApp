768531ed983a0964ffd75389644e8e43baf730fe
Jan Vodák <janvodak92@gmail.com>
2023-10-20 15:20:06 +0200

    added support for applying migration in runtime for development environment in Order microservice

--- src/Services/Order/Order.Rest/Program.cs
+++ src/Services/Order/Order.Rest/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Order.Application.Src;
 using Order.Infrastructure.Src;
+using Order.Infrastructure.Src.Persistence.Context;
+using Order.Infrastructure.Src.Persistence.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,7 @@ builder.Services.AddAplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -19,10 +22,15 @@ if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
 	app.UseSwaggerUI();
+
+	// Migrate the database and seed it - just for the testing purpose
+	app.Services.MigrateDatabase<OrderContext>((context, serviceProvider) =>
+	{
+		OrderContextSeed.SeedAsync(context, serviceProvider.GetRequiredService<ILogger<OrderContextSeed>>()).Wait();
+	});
 }
 
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
