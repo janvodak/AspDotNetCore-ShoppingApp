682cfb548d710cd318590ebef8bff6f635324327
Jan Vodák <janvodak92@gmail.com>
2023-10-26 15:49:19 +0200

    published checkout basket queue message event in basket api

---
+++ src/Services/Basket/Basket.API/Src/Entities/BasketCheckoutEntity.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Basket.API.Src.Entities
+{
+	public class BasketCheckoutEntity
+	{
+		public string UserName { get; set; } = null!;
+
+		[Column(TypeName = "decimal(5, 2)")]
+		public decimal TotalPrice { get; set; }
+
+		// BillingAddress
+		public string FirstName { get; set; } = null!;
+		public string LastName { get; set; } = null!;
+		public string EmailAddress { get; set; } = null!;
+		public string AddressLine { get; set; } = null!;
+		public string Country { get; set; } = null!;
+		public string State { get; set; } = null!;
+		public string ZipCode { get; set; } = null!;
+
+		// Payment
+		public string CardName { get; set; } = null!;
+		public string CardNumber { get; set; } = null!;
+		public string Expiration { get; set; } = null!;
+		public string CardVerificationValue { get; set; } = null!;
+		public int PaymentMethod { get; set; }
+
+		public BasketCheckoutEntity(
+			string userName,
+			decimal totalPrice,
+			string firstName,
+			string lastName,
+			string emailAddress,
+			string addressLine,
+			string country,
+			string state,
+			string zipCode,
+			string cardName,
+			string cardNumber,
+			string expiration,
+			string cardVerificationValue,
+			int paymentMethod)
+		{
+			this.UserName = userName;
+			this.TotalPrice = totalPrice;
+			this.FirstName = firstName;
+			this.LastName = lastName;
+			this.EmailAddress = emailAddress;
+			this.AddressLine = addressLine;
+			this.Country = country;
+			this.State = state;
+			this.ZipCode = zipCode;
+			this.CardName = cardName;
+			this.CardNumber = cardNumber;
+			this.Expiration = expiration;
+			this.CardVerificationValue = cardVerificationValue;
+			this.PaymentMethod = paymentMethod;
+		}
+	}
+}
