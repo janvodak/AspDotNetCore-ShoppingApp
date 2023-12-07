0622273e1f059390ffd3ec3a9a34218efacccf03
Jan Vodák <janvodak92@gmail.com>
2023-12-07 14:24:00 +0100

    added user name into ASP.NET User in Authentication API

--- src/Services/Authentication/Authentication.API/Data/AuthenticationDbContext.cs
+++ src/Services/Authentication/Authentication.API/Data/AuthenticationDbContext.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using ShoppingApp.Services.Authentication.API.Models;
 
 namespace ShoppingApp.Services.Authentication.API.Data
 {
-	public partial class AuthenticationDbContext : IdentityDbContext<IdentityUser>
+	public partial class AuthenticationDbContext : IdentityDbContext<AuthenticationUser>
 	{
 		private readonly IOptions<DatabaseSettings> _databaseSettings;
 
@@ -13,6 +13,8 @@ namespace ShoppingApp.Services.Authentication.API.Data
 		{
 			_databaseSettings = databaseSettings;
 		}
+
+		public DbSet<AuthenticationUser> AuthenticationUsers { get; set; }
 
 		protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
 		{
---
+++ src/Services/Authentication/Authentication.API/Models/AuthenticationUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ShoppingApp.Services.Authentication.API.Models
+{
+	public class AuthenticationUser : IdentityUser
+	{
+		public AuthenticationUser(string firstName, string lastName)
+		{
+			FirstName = firstName;
+			LastName = lastName;
+		}
+
+		public string FirstName { get; set; }
+
+		public string LastName { get; set; }
+	}
+}
