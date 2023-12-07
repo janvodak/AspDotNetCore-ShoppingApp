4b792c2c058f9060f56651b8d243b086c092454c
Jan Vodák <janvodak92@gmail.com>
2023-12-07 13:02:33 +0100

    created default ASP.NET Identity tables

---
+++ src/Services/Authentication/Authentication.API/Data/AuthenticationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace ShoppingApp.Services.Authentication.API.Data
+{
+	public partial class AuthenticationDbContext : IdentityDbContext<IdentityUser>
+	{
+		private readonly IOptions<DatabaseSettings> _databaseSettings;
+
+		public AuthenticationDbContext(IOptions<DatabaseSettings> databaseSettings)
+		{
+			_databaseSettings = databaseSettings;
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+		{
+			dbContextOptionsBuilder.UseSqlServer(_databaseSettings.Value.GetConnectionString());
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
---
+++ src/Services/Authentication/Authentication.API/Data/AuthenticationDbContextMigration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShoppingApp.Services.Authentication.API.Data
+{
+	public class AuthenticationDbContextMigration
+	{
+		private readonly AuthenticationDbContext _applicationDbContext;
+		private readonly ILogger<AuthenticationDbContextMigration> _logger;
+
+		public AuthenticationDbContextMigration(
+			AuthenticationDbContext applicationContext,
+			ILogger<AuthenticationDbContextMigration> logger)
+		{
+			_applicationDbContext = applicationContext;
+			_logger = logger;
+		}
+
+		public async Task MigrateAsync()
+		{
+			IEnumerable<string> _pendingMigrations = _applicationDbContext.Database.GetPendingMigrations();
+
+			if (_pendingMigrations.Any() == true)
+			{
+				await _applicationDbContext.Database.MigrateAsync();
+
+				_logger.LogInformation(
+					"Migrate database associated with context '{DbContextName}'",
+					typeof(AuthenticationDbContext).Name);
+			}
+		}
+	}
+}
