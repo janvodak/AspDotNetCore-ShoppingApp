45945532719d535df867c39890235619974f54e6
Jan Vodák <janvodak92@gmail.com>
2024-02-13 18:32:52 +0100

    added policies into shopping web app to increase resilience

---
+++ src/WebApps/Shopping.WebApp/Policies/Configuration/RetryPolicySettings.cs
@@ -0,0 +1,13 @@
+﻿namespace Shopping.WebApp.Policies.Configuration
+{
+	public class RetryPolicySettings
+	{
+		public const string SECTION_NAME = "RetryPolicy";
+
+		public int MaxRetryAttempts { get; set; }
+
+		public int SecondsBetweenRetries { get; set; }
+
+		public int JittererLimit { get; set; }
+	}
+}
--- src/WebApps/Shopping.WebApp/Program.cs
+++ src/WebApps/Shopping.WebApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Serilog;
 using Shopping.WebApp.Features;
+using Shopping.WebApp.Policies;
+using Shopping.WebApp.Policies.Configuration;
 using Shopping.WebApp.Services;
 using Shopping.WebApp.Settings;
 using ShoppingApp.Components.Logger;
@@ -18,24 +20,43 @@ builder.Services.AddScoped<JsonRequestFactory>();
 ApiServicesSettings apiServicesSettings = new();
 builder.Configuration.GetSection(ApiServicesSettings.NAME_OF_SECTION).Bind(apiServicesSettings);
 
+// Read policy options from configuration
+builder.Services.Configure<CircuitBreakerPolicySettings>(
+	builder.Configuration.GetSection(CircuitBreakerPolicySettings.SECTION_NAME));
+builder.Services.Configure<RetryPolicySettings>(
+	builder.Configuration.GetSection(RetryPolicySettings.SECTION_NAME));
+
+builder.Services.AddScoped<CircuitBreakerPolicyFactory>();
+builder.Services.AddScoped<RetryPolicyFactory>();
+
+// @TODO this is terrible solution, it needs to be rewriten
+CircuitBreakerPolicyFactory circuitBreakerPolicyFactory = builder.Services.BuildServiceProvider().GetRequiredService<CircuitBreakerPolicyFactory>();
+RetryPolicyFactory retryPolicyFactory = builder.Services.BuildServiceProvider().GetRequiredService<RetryPolicyFactory>();
+
 //Add http client services at ConfigureServices(IServiceCollection services)
 builder.Services.AddHttpClient<IProductApiService, ProductApiService>(client =>
 {
 	client.BaseAddress = new Uri(apiServicesSettings.OcelotApiGatewayUrl);
 })
-	.AddHttpMessageHandler<ExternalRecordLoggerDelegatingHandler>();
+	.AddHttpMessageHandler<ExternalRecordLoggerDelegatingHandler>()
+	.AddPolicyHandler(retryPolicyFactory.Create<ProductApiService>())
+	.AddPolicyHandler(circuitBreakerPolicyFactory.Create());
 
 builder.Services.AddHttpClient<IBasketApiService, BasketApiService>(client =>
 {
 	client.BaseAddress = new Uri(apiServicesSettings.OcelotApiGatewayUrl);
 })
-	.AddHttpMessageHandler<ExternalRecordLoggerDelegatingHandler>();
+	.AddHttpMessageHandler<ExternalRecordLoggerDelegatingHandler>()
+	.AddPolicyHandler(retryPolicyFactory.Create<BasketApiService>())
+	.AddPolicyHandler(circuitBreakerPolicyFactory.Create());
 
 builder.Services.AddHttpClient<IOrderApiService, OrderApiService>(client =>
 {
 	client.BaseAddress = new Uri(apiServicesSettings.OcelotApiGatewayUrl);
 })
-	.AddHttpMessageHandler<ExternalRecordLoggerDelegatingHandler>();
+	.AddHttpMessageHandler<ExternalRecordLoggerDelegatingHandler>()
+	.AddPolicyHandler(retryPolicyFactory.Create<OrderApiService>())
+	.AddPolicyHandler(circuitBreakerPolicyFactory.Create());
 
 builder.Services.AddRazorPages();
 
